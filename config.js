e86b197d22146ac5b33370d06973575e45ccb22c
lehoangson143 <52883970+lehoangson143@users.noreply.github.com>
2021-07-01 06:59:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hoàng Anh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Quay lại Inbox với mình nhé công chúa^^.',
+    messDesc: 'Quay lại Inbox với anh nhé công chúa^^.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://facebook.com/lehoangson.vnory'
 }
